c3800f05874cfae30c5ddca4fde041cc2bb1df05
Elijah wines <elijahwines84@yahoo.com>
2020-03-09 21:34:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,8 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
-  def intialize
+  def intialize(name)
+    @name = name
     @balance = 1000
     @status = "open"
   end
