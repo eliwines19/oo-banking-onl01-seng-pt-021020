92ff136fb3bae2cf743d6d7bc668f7fc387abe78
Elijah wines <elijahwines84@yahoo.com>
2020-03-09 22:01:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,20 +13,22 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
+    if @sender.balance < @amount || !valid?
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+    elsif @status == "complete"
+      puts "This transaction has already been processed."
+    else 
+      @sender.deposit(@amount * -1)
+      @receiver.deposit(@amount)
+      @status = "complete"
     end
   end
 
-  def reverse_transfer
+  def reverse_transfer 
     if @status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
+      @sender.deposit(@amount)
+      @receiver.deposit(@amount * -1)
       @status = "reversed"
     end
   end
