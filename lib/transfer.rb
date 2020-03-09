6db3e961353aaaedeb5b09a53585c3ff9634b2a7
Elijah wines <elijahwines84@yahoo.com>
2020-03-09 21:55:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,5 +12,16 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
   end
 
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @reciever.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
 
+  
 end
